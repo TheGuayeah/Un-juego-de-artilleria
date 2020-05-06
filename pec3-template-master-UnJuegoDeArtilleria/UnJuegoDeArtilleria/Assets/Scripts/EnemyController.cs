76b6e60842d0682c64b405a360c1ffd247fd2cf4
TheGuayeah <37774329+TheGuayeah@users.noreply.github.com>
2020-05-06 18:14:00 +0200

    template

---
+++ pec3-template-master-UnJuegoDeArtilleria/UnJuegoDeArtilleria/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class EnemyController : PlayerControl
+{
+    private Transform hero;
+    private float shootingAngle = 30.0f;
+
+	private void Start()
+    {
+        hero = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+    private void LateUpdate()
+    {
+        if (HasTurn)
+        {
+            if (facingRight)
+               Flip();
+
+            if (tilt != shootingAngle)
+            {
+                if (tilt > shootingAngle)
+                    RotaDerechaDown();
+                else
+                    RotaIzquierdaDown();
+            }
+            else
+            {
+                gun.Fire(CalculateVelocity(hero, shootingAngle));
+                RotaDerechaUp();
+                RotaIzquierdaUp();
+            }
+        }
+        else
+        {
+            shootingAngle = Random.Range(25, 45);
+        }
+	}
+
+    private float CalculateVelocity(Transform target, float angle)
+    {
+        var dir = target.position - transform.position;
+        var h = dir.y;
+        dir.y = 0;
+        var dist = dir.magnitude;
+        var a = angle * Mathf.Deg2Rad;
+
+        dist += h / Mathf.Tan(a);
+
+        return Mathf.Sqrt(dist * Physics.gravity.magnitude / Mathf.Sin(2 * a)) * Random.Range(1.2f, 1.8f) * 3;
+    }
+}
---
+++ pec3-template-master-UnJuegoDeArtilleria/UnJuegoDeArtilleria/Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayManager : MonoBehaviour
+{
+    public EnemyController enemy;
+    public PlayerControl hero;
+    public GameObject startButton;
+    public CameraFollow cameraFollow;
+    public GameObject UIRoot;
+
+    public Text mainText;
+    public Text mainTextShadow;
+
+    private int timeRemaining = 10;
+    private GameObject[] ui;
+
+	private void Start()
+    {
+        enemy.GetComponentInChildren<Gun>().gunFired += SwapTurn;
+        hero.GetComponentInChildren<Gun>().gunFired += SwapTurn;
+
+        UIRoot.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        startButton.SetActive(false);
+        enemy.HasTurn = true;
+        InvokeRepeating(nameof(DecreaseTime), 0, 1);
+        SwapTurn();
+    }
+
+    private void SwapTurn()
+    {
+        StartCoroutine(SwapTurnCoroutine());
+    }
+
+    private IEnumerator SwapTurnCoroutine()
+    {
+        timeRemaining = 10;
+        hero.HasTurn = !hero.HasTurn;
+
+        cameraFollow.SetPlayerToFollow(hero.HasTurn ? hero.transform : enemy.transform);
+
+        if (!enemy.HasTurn)
+        {
+            yield return new WaitForSeconds(2.0f);
+        }
+
+        enemy.HasTurn = !enemy.HasTurn;
+    }
+
+    private void DecreaseTime()
+    {
+        timeRemaining--;
+        if (timeRemaining < 0)
+        {
+            SwapTurn();
+        }
+
+        mainText.text = mainTextShadow.text = "" + timeRemaining;
+	}
+}
